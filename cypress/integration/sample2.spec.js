4fb6e05460291dd8adb2c81fc00efb720882ec58
hbarrak <hbarrak@lyance.com>
2019-12-11 16:38:58 +0100

    git push login logout

--- cypress/integration/sample2.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Cypress parallel run example - 2', () => {
-  it('should display the blog link', () => {
-    cy.visit(`https://mherman.org`);
-    cy.get('a').contains('Blog');
-  });
-});
--- cypress/integration/sample3.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Cypress parallel run example - 3', () => {
-  it('should display the about link', () => {
-    cy.visit(`https://mherman.org`);
-    cy.get('a').contains('About');
-  });
-});
